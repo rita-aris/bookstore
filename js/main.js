a84c7f9a55c7d0ec9711e4fe644a8901ed64531d
rita-aris <ritaaris@yandex.ru>
2021-06-02 17:40:39 +0400

    create: added example files

---
+++ js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+  const hotelSwiper = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+  });
+
+  const reviewsSwiper = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    autoHeight: true,
+  });
+
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+  $(".modal__overlay").on("click", closeModal);
+  document.addEventListener("keyup", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+    $("body").css("overflow", "hidden");
+  }
+
+  function closeModal(event) {
+    $("body").css("overflow", "auto");
+    if (event.key === "Escape" || event.type === "click") {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+    event.preventDefault();
+  }
+
+  // Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Your name must be at least 2 letters",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Phone is required",
+          minlength: "Your phone must contain 11 digits",
+        },
+      },
+    });
+  });
+
+  // Маски
+  $(document).ready(function () {
+    $(".phone-mask").mask("+7 (999) 999-99-99");
+  });
+
+  //Анимация
+  AOS.init();
+});
